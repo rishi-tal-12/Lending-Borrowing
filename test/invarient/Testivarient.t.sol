c99cb61ba3ee950b8cd8ce8f8cc7ff47392263a1
Rishi Talreja <rishi_t@me.iitr.ac.in>
2025-03-18 19:49:48 +0000

    This is Lending-Borrowing protocol

--- test/invarient/Testivarient.t.sol
+++ test/invarient/Testivarient.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.28;
 import {Lending} from "src/Lending.sol";
 import {Test} from "lib/forge-std/src/Test.sol";
 import{StdInvariant} from "lib/forge-std/src/StdInvariant.sol";
-contract Invarienttest is Test{
+contract Invarienttest is StdInvariant,Test{
 Lending lending;
 function setUp()public{
 lending=new Lending();
